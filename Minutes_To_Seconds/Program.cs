b07873a1723be9a9f07b431abd4b56f9cc8ca906
Fábio Garcia Rios <fabiogarcia45@gmail.com>
2023-03-13 22:08:54 -0300

    Refactoring program.cs and replacing Convertion.cs into Seconds.cs, Minutes.cs, etc, in single classes with less dependency on each other, following the S (Single responsibility) of the Solid pattern

--- Minutes_To_Seconds/Program.cs
+++ Minutes_To_Seconds/Program.cs
@@ -7,73 +7,58 @@ namespace Minutes_To_Seconds
         static void Main(string[] args)
         {
 
-            double selectedOption = -1;
-            string userInputValue;
-            double validInput;
-            Convertions convertions = new Convertions();
+            string userInputValue = "a";
+            string desiredTimeTypeResult = "a";
+            Seconds secondsConvertion = new Seconds();
 
-            while (selectedOption != 3)
+            while (userInputValue != "exit")
             {
                 Console.Clear();
 
-                Console.WriteLine("Choose an option: \n\n 1 - Minutes to seconds \n 2 - Seconds to minutes \n 3 - Exit program \n");
-                userInputValue = Console.ReadLine();
+                Console.Write("Choose an time type to convert! \n - Seconds \n - Minutes \n - Hours \n - Days \n - Exit - to close the program \n\n");
+                userInputValue = Console.ReadLine().ToLower();
 
-                if (!double.TryParse(userInputValue, out selectedOption))
+                if(userInputValue == "exit")
                 {
-                    //throw new Exception("only numbers are valid");
-                    Console.WriteLine("\nonly numbers are valid!");
+                    Console.WriteLine("\nGoodbye!");
+                    Console.ReadLine();
+                    Environment.Exit(0);
                 }
 
-                switch (selectedOption)
+                Console.Write("\nWrite the type of time you want to receive converted: ");
+                desiredTimeTypeResult = Console.ReadLine().ToLower();
+
+                switch (userInputValue)
                 {
 
-                    case 1:
-                        Console.Write("\nEnter a value (minutes): ");
-                        userInputValue = Console.ReadLine();
-
-                        if (!double.TryParse(userInputValue, out validInput)){
-                            //throw new Exception("only numbers are valid");
-                            Console.WriteLine("only numbers are valid!");
-                            Console.ReadLine();
-                            break;
-                        }
-
-                        convertions.MinutesToSeconds(validInput);
-                        Console.ReadLine();
+                    case "seconds":
+                        secondsConvertion.SecondsTo(desiredTimeTypeResult);
                         break;
 
-                    case 2:
-                        Console.Write("\nEnter a value (seconds): ");
-                        userInputValue = Console.ReadLine();
+                    case "minutes":
 
-                        if (!double.TryParse(userInputValue, out validInput))
-                        {
-                            //throw new Exception("only numbers are valid");
-                            Console.WriteLine("only numbers are valid!");
-                            Console.ReadLine();
-                            break;
-                        }
-
-                        convertions.SecondsToMinutes(validInput);
-                        Console.ReadLine();
                         break;
 
-                    case 3:
-                        Console.WriteLine("\nGoodbye!");
-                        Environment.Exit(0);
+                    case "hours":
+
+                        break;
+
+                    case "days":
+
                         break;
 
                     default:
-                        Console.WriteLine("\nInvalid option!");
+                        Console.WriteLine("Incorrect value! Please type again...");
                         Console.ReadLine();
                         break;
+
                 }
 
+                
+
+                Console.ReadLine();
+
             }
-
-            Console.ReadLine();
-
         }
     }
 }
---
+++ Minutes_To_Seconds/Seconds.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minutes_To_Seconds
+{
+    public class Seconds
+    {
+
+        string userInputValue;
+
+        public void SecondsTo(string desiredConvertionType)
+        {
+            double seconds = 0;
+            double desiredTimeConversion = 0;
+
+            switch (desiredConvertionType.ToLower())
+            {
+                case "minutes":
+                    Console.Write($"Write the number of seconds you want to convert to {desiredConvertionType}: ");
+
+                    userInputValue = Console.ReadLine();
+
+                    if (!double.TryParse(userInputValue, out seconds))
+                    {
+                        //throw new Exception("only numbers are valid");
+                        Console.WriteLine("\nonly numbers are valid!");
+                        break;
+                    }
+
+                    desiredTimeConversion = seconds / 60;
+
+                    if (seconds <= 60)
+                        Console.WriteLine($"\n{seconds} seconds equals {desiredTimeConversion} minute");
+                    else
+                        Console.WriteLine($"\n{seconds} seconds equals {desiredTimeConversion} minutes");
+
+                    break;
+
+
+                case "hours":
+
+                    Console.Write($"Write the number of seconds you want to convert to {desiredConvertionType}: ");
+
+                    userInputValue = Console.ReadLine();
+
+                    if (!double.TryParse(userInputValue, out seconds))
+                    {
+                        //throw new Exception("only numbers are valid");
+                        Console.WriteLine("\nonly numbers are valid!");
+                        break;
+                    }
+
+                    desiredTimeConversion = seconds / 3600;
+
+                    if(seconds <= 3600)
+                        Console.WriteLine($"\n{seconds} seconds equals {desiredTimeConversion} hour");
+                    else
+                        Console.WriteLine($"\n{seconds} seconds equals {desiredTimeConversion} hours");
+
+                    break;
+
+                case "days":
+
+                    Console.Write($"Write the number of seconds you want to convert to {desiredConvertionType}: ");
+
+                    userInputValue = Console.ReadLine();
+
+                    if (!double.TryParse(userInputValue, out seconds))
+                    {
+                        //throw new Exception("only numbers are valid");
+                        Console.WriteLine("\nonly numbers are valid!");
+                        break;
+                    }
+
+                    desiredTimeConversion = seconds / 86400;
+
+                    if(seconds <= 86400)
+                        Console.WriteLine($"\n{seconds} seconds equals {desiredTimeConversion} day");
+                    else
+                        Console.WriteLine($"\n{seconds} seconds equals {desiredTimeConversion} days");
+
+                    break;
+
+                default:
+
+                    Console.WriteLine("Incorrect value! Please type again...");
+
+                    break;
+
+            }
+        }
+
+    }
+}
